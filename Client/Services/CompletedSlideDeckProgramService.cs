eae20f28c9ac1deb5b278d0d23fa9b3904b522b6
Damp Henrik <henrikhemmafru@hotmail.com>
2021-02-14 16:41:14 +0100

    CompletedSlideDeckProgramService refacotring

--- Client/Services/CompletedSlideDeckProgramService.cs
+++ Client/Services/CompletedSlideDeckProgramService.cs
@@ -1,4 +1,5 @@
-﻿using Learning.Client.Shared;
+﻿using Learning.Client.Services.Base;
+using Learning.Client.Shared;
 using Learning.Shared;
 using Learning.Shared.DbModels;
 using System;
@@ -17,42 +18,20 @@ namespace Learning.Client.Services {
 
     public class CompletedSlideDeckProgramService : ICompletedSlideDeckProgramService {
         readonly HttpClient _http;
+        private readonly ServiceBase2 _base;
         public CompletedSlideDeckProgramService(HttpClient http) {
             _http = http;
+            _base = new ServiceBase2(http);
         }
         public async Task<sr<CompletedSlideDeckProgram>> Save(CompletedSlideDeckProgram completedProgram) {
-            var response = await _http.PostAsJsonAsync("api/CompletedSlideDeckProgram", completedProgram);
-            if (response.IsSuccessStatusCode) {
-                //var stream = await response.Content.ReadAsStreamAsync();
-                //var result = await stream.TryDeserializeJsonCamelCaseAsync<CompletedSlideDeckProgram>();
-                //return result;
-                return await ServiceExtensions.ReturnSerializedContentOrError<CompletedSlideDeckProgram>(response);
-            } else {
-                var error = await response.Content.ReadAsStringAsync();
-                return sr<CompletedSlideDeckProgram>.Get(error);
-            }
+            return await _base.Post<CompletedSlideDeckProgram, CompletedSlideDeckProgram>("api/CompletedSlideDeckProgram", completedProgram);
         }
         public async Task<sr<List<CompletedSlideDeckProgram>>> GetAll(UserAvatar userAvatar) {
-            var response = await _http.GetAsync($"api/CompletedSlideDeckProgram/all/{userAvatar.Id}");
-            if (response.IsSuccessStatusCode) {
-                var stream = await response.Content.ReadAsStreamAsync();
-                var result = await stream.TryDeserializeJsonCamelCaseAsync<List<CompletedSlideDeckProgram>>();
-                return result;
-            } else {
-                var error = await response.Content.ReadAsStringAsync();
-                return sr<List<CompletedSlideDeckProgram>>.Get(error);
-            }
+            return await _base.Get<List<CompletedSlideDeckProgram>>($"api/CompletedSlideDeckProgramall/{userAvatar.Id}");
+
         }
         public async Task<sr<CompletedSlideDeckProgram>> GetShared(int id) {
-            var response = await _http.GetAsync($"api/CompletedSlideDeckProgram/shared/{id}");
-            if (response.IsSuccessStatusCode) {
-                var stream = await response.Content.ReadAsStreamAsync();
-                var result = await stream.DeserializeJsonCamelCaseAsync<CompletedSlideDeckProgram>();
-                return sr<CompletedSlideDeckProgram>.GetSuccess(result);
-            } else {
-                var error = await response.Content.ReadAsStringAsync();
-                return sr<CompletedSlideDeckProgram>.Get(error);
-            }
+            return await _base.Get<CompletedSlideDeckProgram>($"api/CompletedSlideDeckProgramall/shared/{id}");
         }
     }
 }
