71c7c3045237ce71d9d08b21bcb8a29d014e1e0f
Damp Henrik <henrikhemmafru@hotmail.com>
2021-03-18 19:48:03 +0100

    reviewcomment added to CompletedProgramReviewable

---
+++ Server/Migrations/20210317194408_azureblob5.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Learning.Server.Migrations
+{
+    public partial class azureblob5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReviewReadByUser",
+                table: "CompletedProgramReviewables",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReviewedComment",
+                table: "CompletedProgramReviewables",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReviewReadByUser",
+                table: "CompletedProgramReviewables");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewedComment",
+                table: "CompletedProgramReviewables");
+        }
+    }
+}
